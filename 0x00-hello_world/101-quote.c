01b856b2c57a1f293663dedc775e616c17058274
Moses-Aduojo <temojokego@gmail.com>
2023-12-14 14:14:58 -0800

    made betty compliant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	/* the constant 2 below can be replaced with the macro "STDERR_FILENO"*/
-	
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
