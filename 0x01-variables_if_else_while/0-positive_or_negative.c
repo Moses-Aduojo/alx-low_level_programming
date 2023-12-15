aa01b8b5df61e964d94d90bf9c07bb994010d14d
Moses-Aduojo <temojokego@gmail.com>
2023-12-15 05:14:52 -0800

    determine if random no_ <, > or = zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always (0) success
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+		puts("is negative");
+	else if (n > 0)
+		puts("is positive");
+	else
+		puts("is zero");
+
+	return (0);
+}
