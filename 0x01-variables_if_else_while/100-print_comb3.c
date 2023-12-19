65f054b23ecbec98eb1cff034267cd1fae6aed8b
Moses-Aduojo <temojokego@gmail.com>
2023-12-18 19:21:15 -0800

    nested while loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (0) success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = i + 1;
+
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 9 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
