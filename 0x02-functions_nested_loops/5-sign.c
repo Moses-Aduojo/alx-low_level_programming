c9d7554f14c30ac61d4919bbce2d34ca047bf1d2
Moses-Aduojo <temojokego@gmail.com>
2023-12-19 15:52:40 -0800

    check and print sign of an number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_sign - Determine sing of n; print (+) if n \
+*is pos, (-) if neg and (0) if zero
+*@n: input num whose sign is determined
+*Return:  1 if n is pos, -1 if neg and (0) if zero
+*/
+
+int print_sign(int n)
+{
+	int i = n;
+
+	if (i > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (i < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
