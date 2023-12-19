154158fbe7277a6ff68903cf3b7c9727ee188725
Moses-Aduojo <temojokego@gmail.com>
2023-12-18 20:53:33 -0800

    more on nested loop

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always (0) success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 98)
+	{
+		int j = 0;
+
+		while (j <= 99)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i < 98 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			++j;
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
