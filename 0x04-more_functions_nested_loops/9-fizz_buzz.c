36c44a72a875ea07f11adec24f6e9c7e275bc9a3
Moses-Aduojo <temojokego@gmail.com>
2023-12-22 13:54:11 -0800

    editted to meet requirement

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,14 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 		i++;
 	}
 	putchar('\n');
