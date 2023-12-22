94872ae4753e77c4a5d8555e68a5a7411f7f613c
Moses-Aduojo <temojokego@gmail.com>
2023-12-22 13:34:15 -0800

    print 0-100. with Fizz, Buzz, for multiple of 3, and 5 resp'vely and FizzBuzz for common multiple of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* main - Entry point, print 1 - 100, fizz, buzz,\
+i* and fizzbuzz for multiple of 3, 5, 3 and 5 respectivly
+* Return: Always (0) success
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
