cd0f23b7558d2a8c9295784d3d682e5fe41af7cc
Moses-Aduojo <temojokego@gmail.com>
2024-01-04 08:58:16 -0800

    editted

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
 * @dest: pointer to destination string
 * @src: pointer to src string
 * @n: number of byte from src string
+* Return: pointer to destination string
 */
 
 char *_strncat(char *dest, char *src, int n)
@@ -10,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 	char *prt = dest;
 	int i;
 
-	while(*prt != '\0')
+	while (*prt != '\0')
 		prt++;
 	i = 0;
 	while (*src != '\0' && i < n)
