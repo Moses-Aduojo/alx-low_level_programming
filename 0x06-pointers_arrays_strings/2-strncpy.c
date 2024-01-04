80183377d8e5680d8524745c209cb523f8b3e98f
Moses-Aduojo <temojokego@gmail.com>
2024-01-04 12:51:20 -0800

    modified to meet requirement

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,26 @@
 /**
-* _strncpy - copy string from src string to a destination string
-* @dest: pointer to destination string
-* @src: pointer to src string
-* @n: number of byte to be copied from src string
-* Return: pointer to dest copy
-*/
-
+ * _strncpy - copy string from src string to a destination string
+ * @dest: pointer to destination string
+ * @src: pointer to src string
+ * @n: number of byte to be copied from src string
+ * Return: pointer to dest copy
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
-	int i = 0;
+    char *ptr = dest;
+    int i;
 
-	while (*src != '\0' && i < n)
-	{
-		*ptr++ = *src++;
-		i++;
-	}
-	if (i < n)
-		*ptr = '\0';
-	return (dest);
+    for (i = 0; i < n && *src != '\0'; i++)
+    {
+        *ptr++ = *src++;
+    }
+
+    while (i < n)
+    {
+        *ptr++ = '\0';
+        i++;
+    }
+
+    return dest;
 }
+
