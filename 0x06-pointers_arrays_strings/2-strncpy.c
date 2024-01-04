87d35b54cea7d5358cb998f137bb42213ab9b14c
Moses-Aduojo <temojokego@gmail.com>
2024-01-04 11:41:41 -0800

    copy n byte from source string to a destination string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+/**
+* _strncpy - copy string from src string to a destination string
+* @dest: pointer to destination string
+* @src: pointer to src string
+* @n: number of byte to be copied from src string
+* Return: pointer to dest copy
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+	int i = 0;
+
+	while (*src != '\0' && i < n)
+	{`
+		*ptr++ = *src++;
+		i++;
+	}
+	if (i < n)
+		*ptr = '\0';
+	return (dest);
+}
