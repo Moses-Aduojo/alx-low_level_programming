5994c45e1358725ee2ea031b890a19cd53cc007e
Moses-Aduojo <temojokego@gmail.com>
2024-01-09 16:11:23 -0800

    editted to meet betty requirement

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 * _memset - fills the first n bytes of the memory area pointed
 * to by s with the constant byte b
 * @s: pointer to area to be filled
-* @b constant by b to be used
+* @b: constant by b to be used
 * @n: number of memory area to be used
 * Return:  a pointer to the memory area s
 */
