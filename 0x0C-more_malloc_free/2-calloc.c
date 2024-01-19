9e26821fa0b5b82cac3f27c0784e44cc81c1ca37
Moses-Aduojo <temojokego@gmail.com>
2024-01-19 11:42:02 -0800

    allocates memory for an array of nmemb elements of size bytes each

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of element of the resulting array
+ * @size: size of each element of the resulting array
+ * Return: pointer the resulting array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t array_size = nmemb * size;
+ 	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(array_size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < array_size; i++)
+	{
+		*((char *)ptr + i) = 0;
+	}
+	return (ptr);
+
+}
