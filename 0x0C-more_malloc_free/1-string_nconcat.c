051c9f257c72e3b4957fea19303d3f559f4f14d7
Moses-Aduojo <temojokego@gmail.com>
2024-01-19 03:44:08 -0800

    concat n byte of string from src to dest string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_nconcat - concatenate n byte from source string to dest
+ * string
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of byte to take from n
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1 = sizeof(*s1);
+	unsigned int i, j;
+	char *ptr, *str1, *str2;
+
+	str1 = s1;
+	str2 = s2;
+	ptr = malloc(size1 + n);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(str1); i++)
+	{
+		ptr[i] = str1[i];
+	}
+
+	for (j = 0; j <= n; j++)
+	{
+		if (str2[j] == '\0')
+			break;
+		ptr[i] = str2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
