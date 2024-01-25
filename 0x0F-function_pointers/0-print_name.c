a372eaf2d629b60295c250ab352a2b0f021a9256
Moses-Aduojo <temojokego@gmail.com>
2024-01-24 20:28:51 -0800

    eddited again

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-
+#include "function_pointers.h"
 
 /**
  * print_name - call a function that print name
@@ -11,6 +11,8 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 	f(name);
 }
