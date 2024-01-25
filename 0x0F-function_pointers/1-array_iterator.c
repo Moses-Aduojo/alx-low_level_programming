8a3b7748ffbbd0e60844234803bbfe5cc7a52f46
Moses-Aduojo <temojokego@gmail.com>
2024-01-24 19:01:32 -0800

    pass array and function to a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter on each\
+ * element of an array.
+ * @array: pointer to the input array
+ * @size: size of the array
+ * @action: pointer to the input function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i <= size; i++)
+	{
+		action(array[i]);
+	}
+}
