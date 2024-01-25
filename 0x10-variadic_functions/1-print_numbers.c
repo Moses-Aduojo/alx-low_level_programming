f15a768fe6c99ff425aebc30fedf9e88efa66904
Moses-Aduojo <temojokego@gmail.com>
2024-01-25 08:36:05 -0800

    variadic  function that prints separated numeric parameters, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numerical paramenter recieved separated by a separator
+ * @separator: used to separate the numbers
+ * @n: numbers of numeric paramenter to be printed
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+	for (i = n; i > 1; i--)
+	{
+		printf("%d%s ", va_arg(ap, int), (separator != NULL) ? separator : "");
+	}
+	printf("%d\n", va_arg(ap, int));
+}
