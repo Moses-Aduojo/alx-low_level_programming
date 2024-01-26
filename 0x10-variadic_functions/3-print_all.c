852390927bf0bc330bf4bd0b3446112e95a74b89
Moses-Aduojo <temojokego@gmail.com>
2024-01-26 15:42:04 -0800

    function that print anything using format string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: pointer to the list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i;
+	char *str;
+
+	va_start(ap, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (i > 0)
+			printf(", ");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("%s", "(nil)");
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+}
