9dd68cf46f86f3bf70ecee9ba03a21f128473309
Moses-Aduojo <temojokego@gmail.com>
2024-02-07 06:19:49 -0800

    editted.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (idx > i)
+	if (idx > i + 1)
 		return (NULL);
 
 	current->next = new_node;
