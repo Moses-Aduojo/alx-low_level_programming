df0640a3fbec4c31f5a17a008cc82270f60d6e31
Moses-Aduojo <temojokego@gmail.com>
2024-02-16 11:56:41 -0800

    write to file, create if it does not exist yet

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		write(2, "empty text\n", 11);
+		/*write(2, "empty text\n", 11);*/
 		return (-1);
 	}
 
 	if (filename == NULL)
 	{
-		write(2, "file name not provided\n", 23);
+		/*write(2, "file name not provided\n", 23);*/
 		return (-1);
 	}
 
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 	{
-		write(2, "can not open file\n", 18);
+		/*write(2, "can not open file\n", 18);*/
 		return (-1);
 	}
 
