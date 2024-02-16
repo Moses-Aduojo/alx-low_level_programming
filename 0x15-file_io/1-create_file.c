bb6a1feeb078204dce81990c2ee0175640430369
Moses-Aduojo <temojokego@gmail.com>
2024-02-16 12:15:44 -0800

    new edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
 	no_char = write(fd, text_content, strlen(text_content));
 	if (no_char < 0)
 	{
