11177811bd35b88843a7cd8e9289f784adee76e2
Moses-Aduojo <temojokego@gmail.com>
2024-03-22 10:59:51 -0700

    editted

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,27 +11,31 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int idx = 0;
 	dlistint_t *temp = head;
 
-	while (temp->next != NULL)
+/**
+ *	while (temp->next != NULL)
+ *	{
+ *		if (idx == index)
+ *			return (temp);
+ *		temp = temp->next;
+ *		idx++;
+ *	}
+ *
+ *	if (temp->next == NULL && idx == index)
+ *		return (temp);
+ */
+
+
+	/**
+	 * this approach is better and eliminate the need for the
+	 * if statement below
+	 */
+	while (temp != NULL)
 	{
 		if (idx == index)
 			return (temp);
 		temp = temp->next;
 		idx++;
 	}
-	/**
-	 * while (temp != NULL)
-	 * {
-	 *	if (idx == index)
-	 *		return (temp);
-	 *	temp = temp->next;
-	 *	idx++;
-	 * }
-	 * Return (NULL);
-	 * this approach is better and eliminate the need for the
-	 * if statement below
-	 */
-	if (temp->next == NULL && idx == index)
-		return (temp);
 
 	return (NULL);
 }
